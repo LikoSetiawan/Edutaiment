3a0011fd8a7ac57a46f55fbfdd355529bae11462
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-02-12 15:02:22 +0800

    Initial commit

---
+++ EdutaimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutaimentApp.swift
+//  Edutaiment
+//
+//  Created by Liko Setiawan on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutaimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
